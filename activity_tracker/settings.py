0fcb86068bca7c2ed67b0c8103462a2b1a053049
Wymagin <wymagin@gmail.com>
2025-05-23 13:23:16 +0200

    Added test branch

--- activity_tracker/settings.py
+++ activity_tracker/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'your-generated-secret-key'
+SECRET_KEY = '!^21=+h%c0b=ijo)ccl&_uh^$5rwg3nf8x4(v&l@d!1l5vf89#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
